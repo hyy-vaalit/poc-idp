55bb14d0947473c7cbbc5ab913cb8b0b28628c15
Petrus Repo <petrus.repo@iki.fi>
2017-01-19 21:50:20 +0200

    Provide student_id in the password field :::)

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -1,16 +1,15 @@
 class SamlIdpController < SamlIdp::IdpController
-  # layout 'saml_idp'
 
-  # def new
-  #   raise params.inspcet
-  # end
-
-  def idp_authenticate(email, password)
-    { email: email }
+  def idp_authenticate(email, not_acually_password)
+    {
+      email: email,
+      student_id: not_acually_password
+    }
   end
 
   def idp_make_saml_response(user)
-    encode_SAMLResponse(user[:email])
+    # dummy response with student number
+    encode_SAMLResponse("urn:schac:personalUniqueCode:fi:yliopisto.fi:#{user[:student_id]}")
   end
 
 end
